b9fb9bdbc9ae3743479cb44079b0bf69aca43a4f
Aruna Baijal <aruna11baijal@gmail.com>
2020-02-21 17:01:05 -0500

    Adding bfs

--- puzzleSolver.py
+++ puzzleSolver.py
@@ -50,6 +50,58 @@ class Node():
             childNode[emptyTile[0], emptyTile[1] - 1] = 0
             return childNode, left
 
+    def bfs(self, goal):
+        open('Nodes.txt', 'w').close()
+        open('NodesInfo.txt', 'w').close()
+        visited = []
+        visited = set(visited)
+        toBeVisited = []
+        toBeVisited.append(self)
+        f = open("Nodes.txt", "a+")
+        f2 = open("NodesInfo.txt", "a+")
+        countMax = 1
+        while(len(toBeVisited)!=0):
+            count = 0
+            visitingNode = toBeVisited.pop(0)
+            node = visitingNode.state
+            toWrite = str(np.transpose(node).reshape(1, 9)[0])
+            f.write(toWrite[1:len(toWrite) - 1] + '\n')
+            f2.write(str(visitingNode.index) + ' ' + str(
+            0 if visitingNode.parent == None else visitingNode.parent.index) + '\n')
+            if(tuple(node.reshape(1, 9)[0]) in visited):
+                continue
+            else:
+                if(np.array_equal(node, goal)):
+                    f.close()
+                    f2.close()
+                    return visitingNode
+                if(visitingNode.moveUp()):
+                    new, up = visitingNode.moveUp()
+                    if not (tuple(new.reshape(1, 9)[0]) in visited):
+                        count += 1
+                        toBeVisited.append(Node(new, visitingNode, countMax+count))
+                if(visitingNode.moveDown()):
+                    new, down = visitingNode.moveDown()
+                    if not (tuple(new.reshape(1, 9)[0]) in visited):
+                        count += 1
+                        toBeVisited.append(Node(new, visitingNode, countMax + count))
+                if(visitingNode.moveRight()):
+                    new, right = visitingNode.moveRight()
+                    if not (tuple(new.reshape(1, 9)[0]) in visited):
+                        count += 1
+                        toBeVisited.append(Node(new, visitingNode, countMax + count))
+                if(visitingNode.moveLeft()):
+                    new, left = visitingNode.moveLeft()
+                    if not (tuple(new.reshape(1, 9)[0]) in visited):
+                        count += 1
+                        toBeVisited.append(Node(new, visitingNode, countMax + count))
+                visited.add(tuple(visitingNode.state.reshape(1, 9)[0]))
+                countMax = countMax + count
+        f.close()
+        f2.close()
+        return False
+
+
 
 
 
